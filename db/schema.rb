640c1c4b185bfe1889618303e465733f7d59b76c
Joseph Mokenela <joseph.mokenela@gmail.com>
2015-03-24 22:11:49 +0200

    Added the Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324193344) do
+ActiveRecord::Schema.define(version: 20150324200635) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20150324193344) do
     t.string   "excerpt"
     t.string   "location"
     t.integer  "user_id"
+  end
+
+  create_table "articles_categories", force: true do |t|
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", force: true do |t|
