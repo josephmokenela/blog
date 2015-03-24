35c0d83f10b84bf42e2b3efff68c1e990fc3aae7
Joseph Mokenela <joseph.mokenela@gmail.com>
2015-03-24 21:38:53 +0200

    Add asociation between articles and users

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   validates_presence_of :title, :body
   
+  belongs_to :user
+  
   def long_title
     "#{title} - #{published_at}"
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :password
   
   has_one :profile
+  has_many :articles
 end
