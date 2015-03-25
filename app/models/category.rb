fb9ff615594259a9bf463aa90ca11267a0428645
Joseph Mokenela <joseph.mokenela@gmail.com>
2015-03-25 11:29:43 +0200

    Add one to many associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_may :articles
+  has_and_belongs_to_many :articles
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :password
   
   has_one :profile
-  has_many :articles -> {order('published_at DESC, title ASC')}, :dependent => :destroy
+  has_many :articles, -> {order('published_at DESC, title ASC')}, :dependent => destroy
 end
