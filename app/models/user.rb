8768ea9f995cd384b79fc56f3efa95038b87f178
Joseph Mokenela <joseph.mokenela@gmail.com>
2015-03-27 14:10:38 +0200

    This actually does the trick, looks like there is a problem with the articles link

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :password
   
   has_one :profiles
-  has_many :articles, -> {order('published_at DESC, title ASC')}, :dependent => destroy
+  #has_many :articles, -> {order('published_at DESC, title ASC')}, :dependent => destroy
   
   #has_many :replies, :through => :articles, :source => :comments
 end
