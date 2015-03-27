cc89c72037a3b17a96c6be5e574121f2b1b6bbce
Joseph Mokenela <joseph.mokenela@gmail.com>
2015-03-27 12:50:50 +0200

    commented out the has_many_through association, My attempt at that is to solve an error or wrong arguments and i try to create a user object

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  #validates_presence_of :email, :password
+  validates_presence_of :email, :password
   
   has_one :profiles
   has_many :articles, -> {order('published_at DESC, title ASC')}, :dependent => destroy
   
-  has_many :replies, :through => :articles, :source => :comments
+  #has_many :replies, :through => :articles, :source => :comments
 end
